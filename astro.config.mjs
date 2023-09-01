731981ab57116a1df395efe5beefece3d5545fcb
Nel Fernandes <46218259+imNel@users.noreply.github.com>
2023-09-01 14:06:58 +0800

    Upgrade to Astro 3.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
   site: "https://nel.fyi",
 });
